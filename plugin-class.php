15f473958a53f422fd8218f915cf7225578a382c
n83b <nathan@websector.com.au>
2017-04-26 14:46:11 +0930

    Added shortcode and updated enqueue

--- plugin-class.php
+++ plugin-class.php
@@ -30,7 +30,7 @@ class WSR_myplugin{
 		add_action( 'wp_enqueue_scripts', array( $this, 'register_plugin_scripts' ) );
 		add_filter( 'single_template', array($this, 'override_single_template' ));
 		add_filter( 'archive_template', array($this, 'override_archive_template' ));
-		add_shortcode('wsr_myplugin', array($this, 'wsr_myplugin_query'));
+		add_shortcode('wsr_myplugin', array($this, 'wsr_myplugin_shortcode'));
 	}
 
 
@@ -112,14 +112,14 @@ class WSR_myplugin{
 	/***********************************************************
 	 * shortcode generic function
 	 */
-	function wsr_myplugin_query($atts){
+	function wsr_myplugin_shortcode($atts){
 
 		$a = shortcode_atts( array(
 	        'element' => '#myplugin'
 	    ), $atts );
 		//$to_use = $a['element'];
 
-		//Enqueue any scripts needed    	
+		//Enqueue any scripts needed - Note, need to register the script first then enqueue here	
 		//wp_enqueue_script( 'thickbox');
 
 		//output html
--- plugin.php
+++ plugin.php
@@ -12,22 +12,42 @@ License: A short license name. Example: GPL2
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-//settings
-$settings = array(
-	'path'	=>  plugins_url() . '/wsr-myplugin',
-	'dir'	=> dirname( __FILE__ ),
-	'currentUrl' => "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"
-);
+//'path'	=>  plugins_url() . '/wsr-myplugin',
+//'dir'	=> dirname( __FILE__ ),
 
-//hooks
+
+
 register_activation_hook( __FILE__, 'wsr_activation');
-register_deactivation_hook(__FILE__, 'wsr_deactivation');
-	
-//functions
 function wsr_activation(){
 
 }
 
+
+
+register_deactivation_hook(__FILE__, 'wsr_deactivation');
 function wsr_deactivation(){
 
 }
+
+
+add_action( 'wp_enqueue_scripts', 'wsr_myplugin_scripts');
+function wsr_myplugin_scripts(){
+	wp_enqueue_script('jquery');
+	wp_register_style( 'wsr-productsearch-css', plugins_url() . '/wsr-myplugin/wsrps.css' );
+	wp_register_script( 'wsr-productsearch-js', plugins_url() . '/wsr-myplugin/wsrps.js', array('jquery'), '20170426', true );
+}
+
+
+
+add_shortcode('wsr_myplugin', 'wsr_myplugin_scripts_shortcode');
+function wsr_myplugin_scripts_shortcode(){
+	//Enqueue any scripts needed   
+	//Note, need to register the script first then enqueue here	
+	wp_enqueue_style( 'wsr-productsearch-css');
+	wp_enqueue_script( 'wsr-productsearch-js'); 
+
+	ob_start(); ?>
+	<p>html goes here</p>
+	<?php return ob_get_clean();
+}
+
