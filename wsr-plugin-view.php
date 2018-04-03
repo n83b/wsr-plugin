3e3a39819d99f6b8c178de389f57f50051faf89d
n8eb <nathan@websector.com.au>
2018-04-03 15:52:51 +0930

    Removed view files

--- wsr-plugin-view.php
+++
@@ -1,9 +0,0 @@
-<div id="wsrplugin"></div>
-
-<script id="wsr-tmpl" type="text/x-handlebars-template">
-	<ul>
-	{{#documents}}
-		<li>{{#if name}} {{name}} {{/if}}</li>
-	{{/documents}}
-	</ul>
-</script>
